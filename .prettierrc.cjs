2778836aa5a782dacdfa7999c4592f8e43e483fd
Myos <myos.chen@gmail.com>
2023-08-19 23:08:11 +0800

    feat: setup tailwindcss

---
+++ .prettierrc.cjs
@@ -0,0 +1,24 @@
+/** @type {import("@ianvs/prettier-plugin-sort-imports").PrettierConfig} */
+module.exports = {
+  singleQuote: true,
+  bracketSpacing: false,
+  plugins: ['@ianvs/prettier-plugin-sort-imports'],
+  importOrder: [
+    '<BUILT_IN_MODULES>',
+    '^(react/(.*)$)|^(react$)',
+    '^(next/(.*)$)|^(next$)',
+    '<THIRD_PARTY_MODULES>',
+    '',
+    '^@/types/(.*)$',
+    '^@/constants/(.*)$',
+    '^@/utils/(.*)$',
+    '^@/hooks/(.*)$',
+    '^@/store/(.*)$',
+    '^@/components/(.*)$',
+    '^@/(.*)$',
+    '',
+    '^[./]',
+  ],
+  importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy'],
+  importOrderTypeScriptVersion: '5.0.2',
+};
